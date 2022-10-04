ba54cbe13e9aad812d13b5b1e4e970fe1537df70
Jason <jm.enns04@gmail.com>
2022-10-04 10:16:01 +0200

    plugin menu button

---
+++ intercra-web/src/components/PluginButton.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <button class="plugin-menu">
+      <img width="25%" src="../assets/three-line.png" class="plugin-menu-image"/>
+      <p id="plugin-menu-text">Show Plugins</p>
+    </button>
+  </div>
+</template>
