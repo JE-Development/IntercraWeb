06a5898e36aba4cdd5e22fef4cab2e933c1077ea
Jason <jm.enns04@gmail.com>
2022-11-11 13:13:38 +0100

    repairing project after setting up new project | changing to typescript

---
+++ intercra-web/src/components/MainSearch.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <div class="center">
+    <div class="search-box">
+      <div class="center-horizontal">
+        <img src="../assets/intercra-text.png" class="logo-text center-horizontal">
+      </div>
+      <input
+          placeholder="Search here"
+          :value="text"
+          class="search-input center-horizontal"
+          @input="event => text = event.target.value">
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  //npm run dev | npm run build
+  name: "MainSearch"
+}
+</script>
+
+<style scoped>
+
+</style>
