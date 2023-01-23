7cc1eea8bab9f53a407e85942e6e7bbe1b3ab27c
LeCraftJay <jm.enns04@gmail.com>
2023-01-23 16:02:26 +0100

    replacing intercra text and lines with one gif

--- src/components/MainSearch.vue
+++ src/components/MainSearch.vue
@@ -11,10 +11,7 @@
     <div class="center">
       <div class="search-box">
         <div class="center-horizontal">
-
-          <img src="../assets/line-bottom.gif" class="logo-text center-horizontal line-bottom absolute hovering-top">
-          <img src="../assets/intercra-text.png" class="logo-text center-horizontal">
-          <img src="../assets/line-top.gif" class="logo-text center-horizontal line-top absolute hovering-bottom">
+          <img src="../assets/intercra_text_anim.gif" class="logo-text center-horizontal">
         </div>
         <input
             @keyup.enter="enterClicked()"
